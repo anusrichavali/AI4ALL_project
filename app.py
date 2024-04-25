275bf790d43f953f828892b61ec275b63a21e75f
Anusri Chavali <chavali.anusri@gmail.com>
2024-04-24 19:18:36 -0700

    updating streamlit

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas_datareader as data
-from keras.models import load_model
+import keras_models
 import streamlit as stream
+from pandas_datareader._utils import RemoteDataError
 
 stream.title('Predicting Stock Prices')
 
 ticker = stream.text_input("Provide name of Stock")
 start_date = '2010-01-01'
-end_date = '2021-07-31' 
+end_date = '2021-12-31' 
+
+try:
+    frame = data.DataReader(ticker, 'yahoo', start_date, end_date)
+    print("Frame:", frame)
+    if frame is not None:
+        if not frame.empty:
+            stream.write(frame.describe())
+        else:
+            raise RemoteDataError
+except Exception as ex:
+    stream.write(f"Error: {ex}")
